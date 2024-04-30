53956dad5f8af3bb56938ad085bd3f12e3720215
VenusBridget <venusbri27@gmail.com>
2024-04-30 17:57:45 +0300

    initial commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  // const linksList = links.map((link) => {
+  //   return <nav><a key ={link} href="#">{link}</a></nav>
+  // })
+  return <nav>
+    {links.map((link) => (
+      <a key ={link} href={"#"+ link}>{link}</a>
+    ))}
+  </nav>
 }
 
 export default NavBar;
